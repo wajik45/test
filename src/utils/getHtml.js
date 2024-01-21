e1f345955502752f50e348735695531dea7b3c97
wajik45 <haikal2589780@gmail.com>
2024-01-21 16:55:31 +0700

    ok

--- src/utils/getHtml.js
+++ src/utils/getHtml.js
@@ -1,6 +1,7 @@
+import axios from "axios";
+
 export default async function getHtml(url) {
-  const response = await fetch(url);
-  const data = await response.text();
+  const { data } = await axios.get(url);
 
   return data;
 }
